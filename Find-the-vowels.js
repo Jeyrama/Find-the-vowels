798dc10ba70a55ec300944d1a5fc8b3a6bb01ce8
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-26 14:50:38 -0700

    Update Find-the-vowels.js

--- Find-the-vowels.js
+++ Find-the-vowels.js
@@ -18,3 +18,12 @@ Notes:
 
 // Solution
 
+function vowelIndices(word) {
+  var arr = [];
+  for(var i = 0; i < word.length; i++) {
+    if(/[aeioyu]/i.test(word[i])) {
+      arr.push(i+1);
+    }
+  }
+  return arr;
+}
